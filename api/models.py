e78ee719ebfe514c23c34e7bc4bc021f02e4beb9
RshmanGit <aarsh.1903@yahoo.com>
2017-12-19 10:26:39 +0530

    added expenses model

--- api/models.py
+++ api/models.py
@@ -16,3 +16,25 @@ class order(models.Model):
     def __str__():
         return '%s %s %d' % (self.name, self.village)
     
+class rawMaterialOrder(models.Model):
+    
+    gatePass = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=200)
+    weight = models.IntegerField()
+    order_date = models.DateField()
+    delivery_date = models.DateField()
+    recieved = models.BooleanField(default = False)
+    
+    def __str__():
+        return '%d %s %s' % (self.gatePass, self.name, self.desc)
+    
+class expenses(models.Model):
+    
+    desc = models.CharField(max_length=200)
+    cost = models.IntegerField()
+    cashInHand = models.IntegerField()
+    date = models.DateField()
+    
+    def __str__():
+        return '%s' % (self.desc)
