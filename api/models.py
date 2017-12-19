396f4585a5bd9bcfa11e5d6ea73e3966e6e01baa
RshmanGit <aarsh.1903@yahoo.com>
2017-12-19 10:19:32 +0530

    actually added order model

--- api/models.py
+++ api/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class order(models.Model):
+    
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    village = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    order_date = models.DateField()
+    delivery_date = models.DateField()
+    delivered = models.BooleanField(default = False)
+    
+    def __str__():
+        return '%s %s %d' % (self.name, self.village)
     
