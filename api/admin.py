cc74e1c2216223c4cbf628344cd9ab7b421d6a0c
RshmanGit <aarsh.1903@yahoo.com>
2017-12-28 12:30:46 +0530

    i was not tracking but so far everything is working except for accepting of post requests

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(order)
+admin.site.register(rawMaterialOrder)
+admin.site.register(expenses)
--- api/resources.py
+++ api/resources.py
@@ -2,36 +2,44 @@ from tastypie.resources import ModelResource
 from api.models import *
 from tastypie.authorization import Authorization
 from datetime import date
+from tastypie.validation import Validation
+from tastypie.authorization import Authorization
 
 class allOrders(ModelResource):
     class Meta:
         queryset = order.objects.all()
         resource_name = 'order'
-        
+        #validation = Validation()
+        authorization = Authorization()
+
 class allRawOrders(ModelResource):
     class Meta:
         queryset = rawMaterialOrder.objects.all()
         resource_name = 'rawMat'
-        
+        #validation = Validation()
+        authorization = Authorization()
+
 class allexps(ModelResource):
     class Meta:
         queryset = expenses.objects.all()
         resource_name = 'exps'
-        
+        #validation = Validation()
+        authorization = Authorization()
+
 class pendingOrder(ModelResource):
     class Meta:
-        temp = order.objects.filter(delivered = False)
+        temp = order.objects.filter(delivered = False).order_by('order_date')
         queryset = temp
         resource_name = 'pendOrder'
 
 class todayRawMat(ModelResource):
     class Meta:
-        temp = rawMaterialOrder.objects.filter(order_date = date.today())
+        temp = rawMaterialOrder.objects.filter(order_date = date.today()).order_by('order_date')
         queryset = temp
         resource_name = 'todayRawMat'
-        
+
 class todayExps(ModelResource):
     class Meta:
-        temp = expenses.objects.filter(date = date.today())
+        temp = expenses.objects.filter(date = date.today()).order_by('date')
         queryset = temp
         resource_name = 'todayExps'
